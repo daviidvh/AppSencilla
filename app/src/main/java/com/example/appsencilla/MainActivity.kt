9a2b09d8c8151cfdd617c3f1759dc56740b1179f
daviidvh <davvel833@alu.centroafuera.es>
2022-10-27 17:52:24 +0200

    Initial commit

---
+++ app/src/main/java/com/example/appsencilla/MainActivity.kt
@@ -0,0 +1,27 @@
+import android.content.Intent
+import android.os.Bundle
+import android.sgoliver.net.holausuario.SaludoActivity
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import com.example.appsencilla.R
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var txtNombre : EditText
+    private lateinit var btnAceptar : Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //Obtenemos una referencia a los controles de la interfaz
+        txtNombre = findViewById(R.id.txtNombre)
+        btnAceptar = findViewById(R.id.btnAceptar)
+        btnAceptar.setOnClickListener {
+            //Creamos el Intent
+            val intent = Intent(this@MainActivity, SaludoActivity::class.java)
+            //Añadimos al intent la información a pasar entre actividades
+            intent.putExtra("NOMBRE", txtNombre.text.toString())
+            //Iniciamos la nueva actividad
+            startActivity(intent)
+        }
+    }
+}
